8b6611c82251ab2c283c2867b442f741ff960f2b
Ddudooo <igoa.work@gmail.com>
2023-12-11 23:47:25 +0900

    feat: day2 part1 solution

--- adventofcode2023/src/main/kotlin/ddudooo/adventofcode2023/day2/part1/Day2Part1Solution.kt
+++ adventofcode2023/src/main/kotlin/ddudooo/adventofcode2023/day2/part1/Day2Part1Solution.kt
@@ -1,7 +1,50 @@
 package ddudooo.adventofcode2023.day2.part1
 
-class Day2Part1Solution {
+class Day2Part1Solution(maxRed: Int = 12, maxGreen: Int = 13, maxBlue: Int = 14) {
+    private val limitGame: Game = Game(maxRed, maxGreen, maxBlue)
+
     fun solution(lines: List<String>): Int {
-        return 8
+        return lines.sumOf { line -> game(line) }
+    }
+
+    private fun game(line: String): Int {
+        return parseLise(line).takeIf { it.isNotOver(limitGame) }?.gameNumber ?: 0
+    }
+
+    private fun parseLise(line: String): Games {
+        val games = line.substringAfter(": ").split("; ").map { game -> parseGame(game) }.toList()
+        val gameNumber = line.substringBefore(": ").substringAfter(" ").toInt()
+        return Games(gameNumber, games)
+    }
+
+    private fun parseGame(line: String): Game {
+        return line.split(", ").map { colorInfo -> parseColorInfo(colorInfo)}.reduce { acc, game -> acc.plus(game) }
+    }
+
+    private fun parseColorInfo(colorInfo: String): Game {
+        val count = colorInfo.substringBefore(" ").toInt()
+        val color = colorInfo.substringAfter(" ")
+        return when (color) {
+            "red" -> Game(red = count)
+            "green" -> Game(green = count)
+            "blue" -> Game(blue = count)
+            else -> Game()
+        }
+    }
+}
+
+data class Games(val gameNumber: Int, val games: List<Game>) {
+    fun isNotOver(limitGame: Game): Boolean {
+        return games.all { it.isNotOver(limitGame) }
+    }
+}
+
+data class Game(val red: Int = 0, val green: Int = 0, val blue: Int= 0) {
+    fun plus(other: Game): Game {
+        return Game(red + other.red, green + other.green, blue + other.blue)
+    }
+
+    fun isNotOver(game: Game): Boolean {
+        return red <= game.red && green <= game.green && blue <= game.blue
     }
 }
