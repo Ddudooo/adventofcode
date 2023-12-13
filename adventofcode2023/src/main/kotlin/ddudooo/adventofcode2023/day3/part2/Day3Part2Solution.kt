3189cf37026ae697b0ad62a6b354f509d9021179
Ddudooo <igoa.work@gmail.com>
2023-12-14 03:15:07 +0900

    add day3 part2 solution
    part1 에서 써먹은 방식을 쓸 수 없어서 다시 짰음
    
    part1을 잘못 푼듯
    
    심볼을 찾은 뒤 근처 숫자를 전부 찾아오는 방식으로 교체(Regex)
    
    찾아온 숫자로 후 처리

--- adventofcode2023/src/main/kotlin/ddudooo/adventofcode2023/day3/part2/Day3Part2Solution.kt
+++ adventofcode2023/src/main/kotlin/ddudooo/adventofcode2023/day3/part2/Day3Part2Solution.kt
@@ -1,7 +1,45 @@
 package ddudooo.adventofcode2023.day3.part2
 
+import kotlin.math.max
+import kotlin.math.min
+
 class Day3Part2Solution {
+
+    private val symbolRegex = Regex("([*])")
+    private val digitRegex = Regex("([0-9]+)")
+
     fun solution(lines: List<String>): Int {
-        return 467835
+        var sum = 0
+        lines.forEachIndexed { rowNum, line ->
+            sum += symbolRegex.findAll(line)
+                .map(MatchResult::range)
+                .map(IntRange::first)
+                .map { symbolIdx ->
+                    nearLines(lines, rowNum)
+                        .flatMap { nearLine -> nearlyDigits(nearLine, symbolIdx) }
+                }
+                .filter { it.size == 2 }
+                .sumOf { it.reduce { acc, i -> i * acc } }
+        }
+
+        return sum
+    }
+
+    private fun nearLines(
+        lines: List<String>,
+        rowNum: Int
+    ): Set<String> {
+        return setOf(
+            lines[max(rowNum - 1, 0)],
+            lines[rowNum],
+            lines[min(rowNum + 1, lines.size - 1)],
+        )
+    }
+
+    private fun nearlyDigits(nearLine: String, symbol: Int): Sequence<Int> {
+        return digitRegex.findAll(nearLine)
+            .map { (it.range.first - 1)..(it.range.last + 1) to it.groupValues[0].toInt() }
+            .filter { (range, value) -> symbol in range }
+            .map { (range, value) -> value }
     }
 }
